67c0dc01e5ea99ca75041426f8d9785bc50bd0b1
jackcizon <jack20021213cn@gmail.com>
2025-12-20 21:53:29 +0800

    fix(algo): fix binary_search test

--- tests/algo/test_binary_search.py
+++ tests/algo/test_binary_search.py
@@ -1,9 +1,8 @@
+from algo.search.binary_search import BinarySearch
+
+
 def test_main():
     arr = [i for i in range(10)]
-    from algo.search.binary_search import BinarySearch
-
-    assert BinarySearch.main(arr=arr, val=7)
-
+    assert BinarySearch.main(arr=arr, val=7) is True
     assert not BinarySearch.main(arr=arr, val=32)
-
     assert not BinarySearch.main(arr=arr, val=5.5)
--- tests/test_demo.py
+++
@@ -1,3 +0,0 @@
-def test_1():
-    a = 1
-    assert a == 1
