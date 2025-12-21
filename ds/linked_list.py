03fc0878c0d0d580dd08a28903b8debc9531a502
jackcizon <jack20021213cn@gmail.com>
2025-12-21 22:46:48 +0800

    refactor(ds): re-write linked_list Node cls

--- ds/linked_list.py
+++ ds/linked_list.py
@@ -1,41 +1,42 @@
+from dataclasses import dataclass
 from typing import Any, Optional, Iterator
-
-
-class Node:
-    def __init__(self, value: Any = None, next_: Optional["Node"] = None) -> None:
-        self.value = value
-        self.next = next_
 
 
 class SingleLinkedList:
     """single linked list with sentinel node"""
 
+    @dataclass(repr=False)
+    class _Node:
+        """private internal dataclass"""
+        value: Any = None
+        next: Optional["SingleLinkedList._Node"] = None
+
     def __init__(self) -> None:
-        self.head: Node = Node(value="__sentinel__", next_=None)
+        self._head = self._Node(value="__sentinel__", next=None)
 
     def push(self, val: Any) -> None:
         """push a new node into list front
         the next pointer order is crucial, or will cause wild pointer"""
-        new_node = Node(value=val, next_=None)
-        new_node.next = self.head.next
-        self.head.next = new_node
+        new_node = self._Node(value=val, next=None)
+        new_node.next = self._head.next  # noinspection PyProtectedMember
+        self._head.next = new_node
 
     def append(self, val: Any) -> None:
         """append a new node into list end"""
-        curr_ptr = self.head
+        curr_ptr = self._head
         while curr_ptr.next:
             curr_ptr = curr_ptr.next
-        new_node = Node(value=val, next_=None)
+        new_node = self._Node(value=val, next=None)
         curr_ptr.next = new_node
 
     def insert(self) -> None:
         raise NotImplementedError("insert() method is no practical usage")
 
     def pop(self) -> Any:
-        if not self.head.next:
+        if not self._head.next:
             return
-        curr_ptr = self.head.next
-        prev_ptr = self.head
+        curr_ptr = self._head.next
+        prev_ptr = self._head
         while curr_ptr.next:
             prev_ptr = curr_ptr
             curr_ptr = curr_ptr.next
@@ -46,7 +47,7 @@ class SingleLinkedList:
         raise NotImplementedError("delete() method is no practical usage")
 
     def __iter__(self) -> Iterator:
-        curr = self.head.next
+        curr = self._head.next
         while curr:
             yield curr.value
             curr = curr.next
