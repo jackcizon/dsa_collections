061943cea0f79e44a0ffaf208e37f3f2f9f01e4c
jackcizon <jack20021213cn@gmail.com>
2025-12-22 00:13:21 +0800

    feat & test: double linked_list

--- tests/ds/test_linked_list.py
+++ tests/ds/test_linked_list.py
@@ -1,6 +1,6 @@
 import pytest
 
-from ds.linked_list import SingleLinkedList
+from ds.linked_list import SingleLinkedList, DoubleLinkedList
 
 
 def test_single_lined_list():
@@ -15,8 +15,8 @@ def test_single_lined_list():
     with pytest.raises(NotImplementedError):
         linked_list.insert()
 
-    val = linked_list.pop()
-    assert val == "world"
+    assert linked_list.pop() == "world"
+    assert linked_list.pop_front() == 0
 
     with pytest.raises(NotImplementedError):
         linked_list.delete()
@@ -26,4 +26,24 @@ def test_single_lined_list():
     linked_list.pop()
     linked_list.pop()
     linked_list.pop()
-    linked_list.pop()
+    linked_list.pop_front()
+
+
+def test_double_linked_list():
+    linked_list = DoubleLinkedList()
+
+    linked_list.is_empty()
+
+    linked_list.push(3)
+    linked_list.push(2)
+    linked_list.push(1)
+    linked_list.push(0)
+
+    linked_list.append(4)
+    linked_list.append(5)
+    linked_list.append(6)
+
+    assert linked_list.pop_front() == 0
+    assert linked_list.pop() == 6
+
+    print(list(linked_list))
