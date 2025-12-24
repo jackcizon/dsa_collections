f3e7801a3b07c4186eb84a2ba96b269a489c0062
jackcizon <jack20021213cn@gmail.com>
2025-12-24 23:47:14 +0800

    feat & test(ds): standard queue

--- ds/queue.py
+++ ds/queue.py
@@ -53,3 +53,63 @@ class BaseQueue:
         for i in range(self._size):
             yield curr.value
             curr = curr.next
+
+
+class StandardQueue:
+    def __init__(self, exp2_len: int) -> None:
+        """standard queue with size=2**k
+        queue may be not cleaned when it's not full,
+        with code logic, we can pass those dirty data"""
+        if exp2_len > 10:
+            exp2_len = 10
+        if exp2_len < 1:
+            exp2_len = 1
+        self._capacity = 2 ** int(exp2_len)
+        self._size = 0
+        self._front = 0
+        self._rear = 0
+        self._buffer = [None] * self._capacity
+
+    def size(self) -> int:
+        return self._size
+
+    def capacity(self) -> int:
+        return self._capacity
+
+    def enqueue(self, val: Any) -> bool:
+        if self.is_full():
+            return False
+        self._buffer[self._rear] = val
+        self._rear = (self._rear + 1) & (self._capacity - 1)
+        self._size += 1
+        return True
+
+    def dequeue(self) -> Any:
+        """may generate dirty data, make inaccessible through code logic"""
+        if self.is_empty():
+            return None
+        val = self._buffer[self._front]
+        self._front = (self._front + 1) & (self._capacity - 1)
+        self._size -= 1
+        return val
+
+    def top(self) -> Any:
+        if self.is_empty():
+            return None
+        return self._buffer[self._front]
+
+    def is_empty(self) -> bool:
+        return self._size == 0
+
+    def is_full(self) -> bool:
+        return self._size == self._capacity
+
+    def __iter__(self) -> Iterator:
+        """fake dequeue to iter"""
+        size = self._size
+        front = self._front
+        while size > 0:
+            yield self._buffer[front]
+            # print(self._buffer[front])
+            front = (front + 1) & (self._capacity - 1)
+            size -= 1
