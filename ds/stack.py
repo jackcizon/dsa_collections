3974eecaa7cbdd01a09f9620257128ebf473fa24
jackcizon <jack20021213cn@gmail.com>
2025-12-25 01:35:30 +0800

    feat & test(ds): standard stack

--- ds/stack.py
+++ ds/stack.py
@@ -54,3 +54,45 @@ class LinkedStack:
         while curr:
             yield curr.value
             curr = curr.next
+
+
+class StandardStack:
+    def __init__(self, capacity: int) -> None:
+        self._capacity = capacity
+        self._size = 0
+        self._buffer = [None] * self._capacity
+
+    def push(self, val: Any) -> bool:
+        if self.is_full():
+            return False
+        self._buffer[self._size] = val
+        self._size += 1
+        return True
+
+    def pop(self) -> Any:
+        if self.is_empty():
+            return None
+        self._size -= 1
+        val = self._buffer[self._size]
+        return val
+
+    def top(self) -> Any:
+        if self.is_empty():
+            return None
+        return self._buffer[self._size - 1]
+
+    def is_empty(self) -> bool:
+        return self._size == 0
+
+    def is_full(self) -> bool:
+        return self._capacity == self._size
+
+    def size(self) -> int:
+        return self._size
+
+    def capacity(self) -> int:
+        return self._capacity
+
+    def __iter__(self) -> Iterator:
+        for i in range(self._size):
+            yield self._buffer[i]
--- tests/ds/test_stack.py
+++ tests/ds/test_stack.py
@@ -1,4 +1,4 @@
-from ds.stack import LinkedStack
+from ds.stack import LinkedStack, StandardStack
 
 
 def test_linked_queue():
@@ -17,6 +17,32 @@ def test_linked_queue():
             print(list(stack))
 
     assert stack.top().value == 5
+
+    print("stack:")
+    print(list(stack))
+    print("start poping:")
+
+    while not stack.is_empty():
+        print(f"pop-ed: {stack.pop()}, remains", list(stack))
+    print(list(stack))
+
+
+def test_standard_stack():
+    stack = StandardStack(6)
+    print(list(stack))
+
+    assert stack.size() == 0
+    assert stack.capacity() == 6
+    assert stack.top() is None
+    assert stack.pop() is None
+
+    print("start pushing:")
+    for i in range(100):
+        stack.push(i)
+        if not stack.is_full():
+            print(list(stack))
+
+    assert stack.top() == 5
 
     print("stack:")
     print(list(stack))
