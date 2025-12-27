57f46905f7920773dd507784de56d2bdb4c440fc
jackcizon <jack20021213cn@gmail.com>
2025-12-27 21:50:32 +0800

    feat & test(ds): heap sort example

---
+++ ds/examples/heap_example.py
@@ -0,0 +1,13 @@
+from typing import Any
+
+from ds.heap import MaxHeap
+
+
+def kth_largest(arr: list, k: int) -> Any:
+    if k < 1 or k > len(arr):
+        raise ValueError("k is out of range")
+    heap = MaxHeap(arr)
+    while k > 1:
+        heap.pop()
+        k -= 1
+    return heap.pop()
